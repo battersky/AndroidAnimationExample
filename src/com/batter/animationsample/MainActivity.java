49dd54e72f33205fc82b66ae09aa5f3cced2e581
sky batter <batter.sky@gmail.com>
2014-01-26 23:31:23 +0800

    base version

---
+++ src/com/batter/animationsample/MainActivity.java
@@ -0,0 +1,28 @@
+package com.batter.animationsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView scale_view = (ImageView) this.findViewById(R.id.scale_view);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.translate_test);
+        scale_view.startAnimation(animation);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
